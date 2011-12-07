9168d266a4f3cadbd355e268c0292cc7189e2fa8
Aaron Patterson <aaron.patterson@gmail.com>
2011-12-06 17:17:14 -0800

    reducing diff from trunk

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -559,6 +559,7 @@ vm_call_method(rb_thread_t *th, rb_control_frame_t *cfp,
     VALUE val;
 
   start_method_dispatch:
+
     if (me != 0) {
 	if ((me->flag == 0)) {
 	  normal_method_dispatch:
@@ -706,7 +707,6 @@ vm_call_method(rb_thread_t *th, rb_control_frame_t *cfp,
 	if (flag & VM_CALL_SUPER_BIT) {
 	    stat |= NOEX_SUPER;
 	}
-
 	if (id == idMethodMissing) {
 	    VALUE *argv = ALLOCA_N(VALUE, num);
 	    vm_method_missing_args(th, argv, num - 1, 0, stat);
