b01c5fccf39073851b71d993eae61c8bd98f0d82
Aaron Patterson <aaron.patterson@gmail.com>
2012-05-01 14:15:09 -0700

    object create start probes converted

--- test/dtrace/test_object_create_start.rb
+++ test/dtrace/test_object_create_start.rb
@@ -2,67 +2,66 @@ require 'dtrace/helper'
 
 module DTrace
   class TestObjectCreateStart < TestCase
-    def test_object_create_start
-      probe = <<-eoprobe
-ruby#{$$}:::object-create-start
+    def probe
+      <<-eoprobe
+ruby$target:::object-create-start
 {
   printf("%s %s %d\\n", copyinstr(arg0), copyinstr(arg1), arg2);
 }
       eoprobe
-      saw = trap_probe(probe) { 10.times { Object.new } }
-      assert_equal 10, saw.length
+    end
+
+    def test_object_create_start
+      trap_probe(probe, '10.times { Object.new }') { |_,rbfile,saw|
+        saw = saw.map(&:split).find_all { |_, file, _|
+          file == rbfile
+        }
+        assert_equal 10, saw.length
+      }
     end
 
     def test_object_create_start_name
-      probe = <<-eoprobe
-ruby#{$$}:::object-create-start
-{
-  printf("%s %s %d\\n", copyinstr(arg0), copyinstr(arg1), arg2);
-}
-      eoprobe
-      saw, line = trap_probe(probe) { Hash.new }, __LINE__
-      assert_equal(%w{ Hash }, saw.map { |line| line[/^\w+/] })
-      assert_equal([__FILE__], saw.map { |line| line.split[1] })
-      assert_equal([line.to_s], saw.map { |line| line.split[2] })
+      trap_probe(probe, 'Hash.new') { |_,rbfile,saw|
+        saw = saw.map(&:split).find_all { |klass, file, line|
+          file == rbfile
+        }
+        assert_equal(%w{ Hash }, saw.map(&:first))
+        assert_equal([rbfile], saw.map { |line| line[1] })
+        assert_equal(['1'], saw.map { |line| line[2] })
+      }
     end
 
     def test_object_create_start_hash_lit
-      probe = <<-eoprobe
-ruby#{$$}:::object-create-start
-{
-  printf("%s %s %d\\n", copyinstr(arg0), copyinstr(arg1), arg2);
-}
-      eoprobe
-      saw, line = trap_probe(probe) { {} }, __LINE__
-      assert_equal(%w{ Hash }, saw.map { |line| line[/^\w+/] })
-      assert_equal([__FILE__], saw.map { |line| line.split[1] })
-      assert_equal([line.to_s], saw.map { |line| line.split[2] })
+      trap_probe(probe, '{}') { |_,rbfile,saw|
+        saw = saw.map(&:split).find_all { |klass, file, line|
+          file == rbfile
+        }
+        assert_equal(%w{ Hash }, saw.map(&:first))
+        assert_equal([rbfile], saw.map { |line| line[1] })
+        assert_equal(['1'], saw.map { |line| line[2] })
+      }
     end
 
     def test_object_create_start_array_lit
-      probe = <<-eoprobe
-ruby#{$$}:::object-create-start
-{
-  printf("%s %s %d\\n", copyinstr(arg0), copyinstr(arg1), arg2);
-}
-      eoprobe
-      saw, line = trap_probe(probe) { [] }, __LINE__
-      assert_equal(%w{ Array }, saw.map { |line| line[/^\w+/] })
-      assert_equal([__FILE__], saw.map { |line| line.split[1] })
-      assert_equal([line.to_s], saw.map { |line| line.split[2] })
+      trap_probe(probe, '[]') { |_,rbfile,saw|
+        saw = saw.map(&:split).find_all { |klass, file, line|
+          file == rbfile
+        }
+        assert_equal(%w{ Array }, saw.map(&:first))
+        assert_equal([rbfile], saw.map { |line| line[1] })
+        assert_equal(['1'], saw.map { |line| line[2] })
+      }
     end
 
     def test_object_create_start_string_lit
-      probe = <<-eoprobe
-ruby#{$$}:::object-create-start
-{
-  printf("%s %s %d\\n", copyinstr(arg0), copyinstr(arg1), arg2);
-}
-      eoprobe
-      saw, line = trap_probe(probe) { "omg" }, __LINE__
-      assert_equal(%w{ String }, saw.map { |line| line[/^\w+/] })
-      assert_equal([__FILE__], saw.map { |line| line.split[1] })
-      assert_equal([line.to_s], saw.map { |line| line.split[2] })
+      trap_probe(probe, '"omg"') { |_,rbfile,saw|
+        saw = saw.map(&:split).find_all { |klass, file, line|
+          file == rbfile
+        }
+        assert_equal(%w{ String }, saw.map(&:first))
+        assert_equal([rbfile], saw.map { |line| line[1] })
+        assert_equal(['1'], saw.map { |line| line[2] })
+      }
     end
   end
 end
