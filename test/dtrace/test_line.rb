fac49bffb4c7d75dfcfc3ea184b48a20a51fd4df
Aaron Patterson <aaron.patterson@gmail.com>
2012-05-01 14:34:03 -0700

    testing line probes

--- test/dtrace/test_line.rb
+++ test/dtrace/test_line.rb
@@ -3,16 +3,16 @@ require 'dtrace/helper'
 module DTrace
   class TestLine < TestCase
     def test_line
-      probe = "ruby#{$$}:::line { printf(\"%s %d\\n\", copyinstr(arg0), arg1); }"
+      probe = "ruby$target:::line { printf(\"%s %d\\n\", copyinstr(arg0), arg1); }"
+      program = 'x = 2; 10.times { x = x ** 2 }'
 
-      x = 2
-      saw, line = trap_probe(probe) { 10.times { x = x ** 2 } }, __LINE__
-      saw.map!(&:split)
-
-      this = saw.find_all { |row| row.first == __FILE__ }
-
-      assert_operator this.length, :>=, 10
-      this.each { |f,l| assert_equal line.to_s, l }
+      trap_probe(probe, program) { |_, rbpath, saw|
+	saw = saw.map(&:split).find_all { |file, _|
+	  file == rbpath
+	}
+	assert_operator saw.length, :>=, 10
+	saw.each { |f,l| assert_equal '1', l }
+      }
     end
   end
 end
