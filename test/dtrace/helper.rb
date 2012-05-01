7e97fca8dac6c25df7b9c169c9fcbff806976f63
Aaron Patterson <aaron.patterson@gmail.com>
2012-05-01 11:12:18 -0700

    converted one test to eval ruby

--- test/dtrace/helper.rb
+++ test/dtrace/helper.rb
@@ -7,19 +7,25 @@ module DTrace
       skip "must be setuid 0 to run dtrace tests" unless Process.euid == 0
     end
 
-    def trap_probe code
-      f = Tempfile.new('omg.d')
-      f.write code
-      f.puts
-      f.write 'tick-1hz { exit(0); }'
-      f.flush
-      probes = IO.popen("dtrace -q -s #{f.path}") do |io|
-        sleep 0.5
-        yield
-        io.readlines.grep(/^\S+/)
+    def trap_probe d_program, ruby_program
+      d = Tempfile.new('probe.d')
+      d.write d_program
+      d.flush
+
+      rb = Tempfile.new('probed.rb')
+      rb.write ruby_program
+      rb.flush
+
+      d_path  = d.path
+      rb_path = rb.path
+
+      cmd = "dtrace -q -s #{d_path} -c '#{Gem.ruby} #{rb_path}'"
+      probes = IO.popen(cmd) do |io|
+        io.readlines
       end
-      f.close(true)
-      probes
+      d.close(true)
+      rb.close(true)
+      yield(d_path, rb_path, probes)
     end
   end
 end
--- test/dtrace/test_function_entry.rb
+++ test/dtrace/test_function_entry.rb
@@ -2,42 +2,54 @@ require 'dtrace/helper'
 
 module DTrace
   class TestFunctionEntry < TestCase
-    def foo; end
-
     def test_function_entry
       probe = <<-eoprobe
-ruby#{$$}:::function-entry
+ruby$target:::function-entry
 {
   printf("%s %s %s %d\\n", copyinstr(arg0), copyinstr(arg1), copyinstr(arg2), arg3);
 }
       eoprobe
-      saw = trap_probe(probe) { 10.times { foo } }
-      foo_calls = saw.map { |line| line.split }.find_all { |row|
-	row.first == self.class.name && row[1] == 'foo'
-      }
 
-      assert_equal 10, foo_calls.length
-      line = method(:foo).source_location[1].to_s
-      foo_calls.each { |f| assert_equal line, f[3] }
-      foo_calls.each { |f| assert_equal __FILE__, f[2] }
+      trap_probe(probe, ruby_program) { |d_file, rb_file, probes|
+	foo_calls = probes.map { |line| line.split }.find_all { |row|
+	  row.first == 'Foo'  && row[1] == 'foo'
+	}
+
+	assert_equal 10, foo_calls.length
+	line = '2'
+	foo_calls.each { |f| assert_equal line, f[3] }
+	foo_calls.each { |f| assert_equal rb_file, f[2] }
+      }
     end
 
     def test_function_return
       probe = <<-eoprobe
-ruby#{$$}:::function-return
+ruby$target:::function-return
 {
   printf("%s %s %s %d\\n", copyinstr(arg0), copyinstr(arg1), copyinstr(arg2), arg3);
 }
       eoprobe
-      saw = trap_probe(probe) { 10.times { foo } }
-      foo_calls = saw.map { |line| line.split }.find_all { |row|
-	row.first == self.class.name && row[1] == 'foo'
-      }
 
-      assert_equal 10, foo_calls.length
-      line = method(:foo).source_location[1].to_s
-      foo_calls.each { |f| assert_equal line, f[3] }
-      foo_calls.each { |f| assert_equal __FILE__, f[2] }
+      trap_probe(probe, ruby_program) { |d_file, rb_file, probes|
+	foo_calls = probes.map { |line| line.split }.find_all { |row|
+	  row.first == 'Foo'  && row[1] == 'foo'
+	}
+
+	assert_equal 10, foo_calls.length
+	line = '2'
+	foo_calls.each { |f| assert_equal line, f[3] }
+	foo_calls.each { |f| assert_equal rb_file, f[2] }
+      }
+    end
+
+    def ruby_program
+      <<-eoruby
+      class Foo
+	def foo; end
+      end
+      x = Foo.new
+      10.times { x.foo }
+      eoruby
     end
   end
 end
